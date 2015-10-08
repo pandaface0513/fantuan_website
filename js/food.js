6319823b7c79ba91c38a73eb7247467c9ffcea91
Henry Lo <risenhell@gmail.com>
2015-10-08 00:22:24 -0700

    Pizza pop up looks good

--- js/food.js
+++ js/food.js
@@ -2,12 +2,40 @@ var bone = 12.95;
 var tofu = 9.95;
 var mushroom = 15.95;
 
+var otherValue = 0;
+
 //to start all the listeners
 $(document).ready(function(){
     $('.foodFilter').click(function(){
         $('.foodFilter').removeClass("selected");
         $(this).toggleClass("selected");
         $(".foodGridItem").hide().fadeIn('slow');
+    });
+    
+    $('.topFiltersButton').click(function(){
+        $('.topFiltersButton').removeClass("selected");
+        $(this).toggleClass("selected");
+    });
+    
+    $('.sizeFiltersButton').click(function(){
+        $('.sizeFiltersButton').removeClass("selected");
+        $(this).toggleClass("selected");
+    });
+    
+    $('#exitFood').click(function(){
+       $("#page-cover").css("opacity",0.6).fadeOut(300);
+        $('#foodDetails').hide();
+    });
+    
+    $('#addFood').click(function(){
+       $("#page-cover").css("opacity",0.6).fadeOut(300);
+        $('#foodDetails').hide();
+        $('#cartItems').append("<li class='orderItem'><p>" + "Pizza" + "<br>$8.99</p><div class='right'><input class='newQuantity' id='mushroomCounter' type='number' name='bone' value='1' min='1' max='99'></div></li>");
+
+            $('.newQuantity').bootstrapNumber();
+            $('.newQuantity').addClass('quantity').removeClass('newQuantity');
+
+            otherValue++;
     });
     
     /*<img src="img/restaurant.jpg">*/
@@ -37,23 +65,43 @@ $(document).ready(function(){
            console.log(mushroomValue);
        }
         
-        var sum = (boneValue * bone) + (tofuValue * tofu) + (mushroomValue * mushroom);
+        var sum = (boneValue * bone) + (tofuValue * tofu) + (mushroomValue * mushroom) + (8.99 * otherValue);
         sum = sum.toFixed(2);
         
         console.log(sum);
         
         $('#sum').html("餐费总额：$" + sum);
         
-        var count = boneValue * 1 + tofuValue * 1 + mushroomValue * 1;
+        var count = boneValue * 1 + tofuValue * 1 + mushroomValue * 1 + otherValue * 1;
         $('#cartCount').html(count);
         
     });
     
-    $('.foodGridImage img').bind('mouseenter mouseleave', function(){
-       $(this).attr({
-          src: $(this).attr('data-other-src'),
-           'data-other-src': $(this).attr('src')
-       })
+    $('.foodGridItem').bind('mouseenter mouseleave', function(){
+        $(this).find("img").attr({
+          src: $(this).find("img").attr('data-other-src'),
+           'data-other-src': $(this).find("img").attr('src')
+        });
+    });
+    
+    $('.foodGridItem').click(function(){
+/*       var foodName = $(this).find("foodInfo").innerHTML;*/
+        
+/*        console.log(foodName);*/
+        if($(this).hasClass("specialPop")){
+            $("#page-cover").css("opacity",0.6).fadeIn(300);
+            $('#foodDetails').show();
+            $('#foodDetails').css({'position':'aboslute','z-index':9999});
+        }
+        else{
+            $('#cartItems').append("<li class='orderItem'><p>" + "锅包肉" + "<br>$8.99</p><div class='right'><input class='newQuantity' id='mushroomCounter' type='number' name='bone' value='1' min='1' max='99'></div></li>");
+
+            $('.newQuantity').bootstrapNumber();
+            $('.newQuantity').addClass('quantity').removeClass('newQuantity');
+
+            otherValue++;
+        }
+        
     });
     
 });
